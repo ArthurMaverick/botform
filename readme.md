f39b7f770d08d299a3d9ea0e2b91d0660f6985e8
ArthurMaverick <arthuracs18@gmail.com>
2021-10-08 15:00:22 -0300

    readme bot

--- readme.md
+++ readme.md
@@ -15,15 +15,18 @@ nivel **tiger**
 - opção de escolher o tempo de cada envio
 - opção de escolher os inputs do formulário
 
+
 nivel **demon**
 - ui com sessao e acesso a outros privilegios de percistencia
 - templates de opçao da escolha de tempo, de forma linear e não linear 
 - opção de escolher os inputs do formulário de forma simplificada
+
 
 nivel **dragon** 
 - ui complexa com logs do bot e todos privilegios anteriores
 - templates de opçao da escolha de tempo, de forma linear e nao linear
 - opção de se adptar aos warnings do server
 - habilidade de passar por mais barreiras de segurança
+
 
 ### NÍVEL ATUAL:  **_WOLF_**
--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,20 @@ let limitCounter: number = 1
 if (limitCounter < 9_000){
   setInterval(
     async () => {
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto('https://docs.google.com/forms/d/e/1FAIpQLSdK_JYYOMZulfSPqNxqJP4Zrkjb7XxTn3Cd1mPfw16qQ1kUJQ/viewform?fbzx=1421041915633150429', {
       waitUntil: 'networkidle2',
     });
     page.once('load', () => console.log('Page loaded!'));
     await page.type('input[jsname="YPqjbf"]', 'test@example.com');
-    await page.click('#i33')
-    await page.click('#i94')
+    await page.click('#i9')
+    await page.click('#i43')
     await page.click('div[jsname="M2UYVd"]')
+    
     await browser.close()
-
     console.log('enviado', ++limitCounter, 'vezes' )
-}, 4_000);
+  }, 6_000);
 } else {
   console.log('Acabou!')
 }
