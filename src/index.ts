08b4393b7a42ab44ec9a96d1aa2da2be65ccb0d0
ArthurMaverick <arthuracs18@gmail.com>
2021-10-08 18:06:51 -0300

    move to once load method id page

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,19 @@ let limitCounter: number = 1
 if (limitCounter < 9_000){
   setInterval(
     async () => {
-    const browser = await puppeteer.launch({headless: true});
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://docs.google.com/forms/d/e/1FAIpQLSdK_JYYOMZulfSPqNxqJP4Zrkjb7XxTn3Cd1mPfw16qQ1kUJQ/viewform?fbzx=1421041915633150429', {
       waitUntil: 'networkidle2',
     });
-    page.once('load', () => console.log('Page loaded!'));
+    page.once('load', async() => {
+      console.log('Page loaded!')
+      await page.type('input[jsname="YPqjbf"]', 'test@example.com');
+      await page.click('#i9')
+      await page.click('#i43')
+      await page.click('div[jsname="M2UYVd"]')
+    });
+
     await page.type('input[jsname="YPqjbf"]', 'test@example.com');
     await page.click('#i9')
     await page.click('#i43')
